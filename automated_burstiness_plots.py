6a1a3f6f76dfb958b3b4e70706f140d55a1df5ee
undomielren <93618055+undomielren@users.noreply.github.com>
2022-03-02 13:32:47 -0600

    Update automated_burstiness_plots.py

--- automated_burstiness_plots.py
+++ automated_burstiness_plots.py
@@ -19,7 +19,8 @@ rogue_halo_dir_path = "/myhome2/users/azartash/sncalc/rogue_halos/"
     #storm_halos = [1,2,3,4,5,6,7,8,10,11,12,14,15,22,23,31,37,44,48,55,118]
     #rogue_halos = [1,3,7,8,10,11,12,15,16,17,28,31,37,58,116] 
 
-#uncomment SIM you want to plot:
+###---uncomment SIM you want to plot:---###
+
 #CptM_Data = pickle.load(open(cptm_halo_dir_path + 'cpt_marvel_burstiness.pickle', 'rb'))
 #cm_burstiness, cm_t_burst = [],[]
 #for h in ['1','2','3','5','6','7','10','11','13','14','24']:
@@ -38,11 +39,11 @@ rogue_halo_dir_path = "/myhome2/users/azartash/sncalc/rogue_halos/"
     #s_burstiness = Storm_Data[h]['Burstiness']    
     #s_t_burst = Storm_Data[h]['Burstiness_bins']  
 
-Rogue_Data = pickle.load(open(rogue_halo_dir_path + 'rogue_burstiness.pickle', 'rb'))
-r_burstiness, r_t_burst = [],[]
-for h in ['1','3','7','8','10','11','12','15','16','17','28','31','37','58','116'] :
-    r_burstiness = Rogue_Data[h]['Burstiness']
-    r_t_burst = Rogue_Data[h]['Burstiness_bins']
+#Rogue_Data = pickle.load(open(rogue_halo_dir_path + 'rogue_burstiness.pickle', 'rb'))
+#r_burstiness, r_t_burst = [],[]
+#for h in ['1','3','7','8','10','11','12','15','16','17','28','31','37','58','116'] :
+    #r_burstiness = Rogue_Data[h]['Burstiness']
+    #r_t_burst = Rogue_Data[h]['Burstiness_bins']
     
     print("Creating plots...")
     # Plotting of star formation rate and burstiness
